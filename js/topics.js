bf48c1722830a0a16dd765c7ea7d69239f2a35b5
nukunukuland <charo.charo.wanwan0316@gmail.com>
2025-08-10 22:46:59 +0900

    Update topics.js

--- js/topics.js
+++ js/topics.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const topicsData = [
     { date: "2025.07.30", catClass: "cat04", catLink: "/homepage/topics/cat04", title: "ホームページを公開しました。", link: "#" },
-    { date: "2025.08.02", catClass: "cat01", catLink: "/homepage/topics/cat02", title: "つながりあそび・うた​研究所35周年記念コンサートのお知らせ", link: "#" },
+    { date: "2025.08.02", catClass: "cat01", catLink: "/homepage/topics/cat01", title: "つながりあそび・うた​研究所35周年記念コンサートのお知らせ", link: "#" },
     { date: "2025.08.04", catClass: "cat04", catLink: "/homepage/topics/cat04", title: "カレンダー更新しました【不定期更新】", link: "#" }
 
   ];
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
   }).join("");
 });
+
 
 
 
